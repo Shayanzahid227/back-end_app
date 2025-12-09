810c15a1dddc9a8611f6b5831f8f177291263c26
Shayanzahid227 <sshayanzahid227@gmail.com>
2025-12-09 17:49:59 +0500

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:hustler_syn/core/constant/colors.dart';
 import 'package:hustler_syn/firebase_options.dart';
+import 'package:hustler_syn/screens/profile/main_profile/profile_screen.dart';
 import 'package:hustler_syn/screens/root_screen/root_screen.dart';
 
 void main() async {
@@ -57,7 +58,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          home: RootScreen(),
+          home: ProfileScreen(),
         );
       },
     );
